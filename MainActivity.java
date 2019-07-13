4a218f5db6baf3e82930c9e03fdc916dc056f89d
PranavR149 <52850366+PranavR149@users.noreply.github.com>
2019-07-13 15:21:56 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,36 +1,49 @@
-package com.example.demo;
+package com.example.guessthenumber;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final Random RANDOM = new Random();
-    public View root;
-    public Button button;
+    int n;
 
+    public void makeToast(String s) {
+
+        Toast.makeText(MainActivity.this,s, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void guess(View view) {
+
+        EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
+
+        int guessInt = Integer.parseInt(guessEditText.getText().toString());
+        if (guessInt > n) {
+        makeToast("Lower..!!");
+        } else if (guessInt < n) {
+        makeToast("Higher..!!");
+        } else {
+        makeToast("That's Right:-),Try Again..!!");
+            Random rand = new Random();
+            n = rand.nextInt(20)+1;
+
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        root = findViewById(R.id.root);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int red = RANDOM.nextInt(255);
-                int green = RANDOM.nextInt(255);
-                int blue = RANDOM.nextInt(255);
 
-                int color = Color.rgb(red,green,blue);
-                root.setBackgroundColor(color);
-            }
-        });
+        Random rand = new Random();
+        int n = rand.nextInt(20)+1;
     }
 }
